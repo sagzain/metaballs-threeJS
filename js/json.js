aa76dce7535ecd6d336c7be019156b29b94e5ea3
Samuglz6 <Samuel.Gonzalez3@alu.uclm.es>
2020-05-15 17:04:41 +0200

    testing with geometry

--- js/json.js
+++
@@ -1,2 +0,0 @@
-//Json is created as a variable in a js file in order to be easily imported
-var json = '[{"type": "grid","value": 0.1,"threshold": 0.6},{"type": "blob","position": [0.0, 0.0, 0.0],"energy": 6,"radius": 1},{"type": "blob","position": [0.75, 0.75, 0.75],"energy": 4, "radius": 1},{"type": "blob","position": [-0.5, -0.75, 0.3],"energy": 4,"radius": 1}]';
